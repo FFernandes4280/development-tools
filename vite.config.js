5268c9f25f396507ac1a7ba0948f9edd49676210
Filipe Castro Fernandes <filipecfernandes2004@gmail.com>
2026-07-01 11:29:54 -0300

    nodePolyfills is needed

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import path from 'path'
 
 export default defineConfig({
   plugins: [
     react(),
+    nodePolyfills()
   ],
   build: {
     lib: {
